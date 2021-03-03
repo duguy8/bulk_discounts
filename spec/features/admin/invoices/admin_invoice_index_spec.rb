ffbf87b3ac9d58e1c71b1de7bf992c1d750ef084
Wil McCauley <wmccauley724@gmail.com>
2021-03-02 18:01:39 -0700

    Pass test with admin invoice index page

---
+++ spec/features/admin/invoices/admin_invoice_index_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe "When I visit the admin Invoices index" do
+  before :each do
+    @invoices = create_list(:invoice, 5)
+  end
+
+  it "displays all invoice ids" do
+    visit admin_invoices_path
+
+    expect(page).to have_content(@invoices.first.id)
+    expect(page).to have_content(@invoices.second.id)
+    expect(page).to have_content(@invoices.third.id)
+    expect(page).to have_content(@invoices.fourth.id)
+    expect(page).to have_content(@invoices[4].id)
+
+    expect(page).to have_link("#{@invoices.first.id}")
+    expect(page).to have_link("#{@invoices.second.id}")
+    expect(page).to have_link("#{@invoices.third.id}")
+    expect(page).to have_link("#{@invoices.fourth.id}")
+    expect(page).to have_link("#{@invoices[4].id}")
+  end
+end
+
+# As an admin,
+# When I visit the admin Invoices index ("/admin/invoices")
+# Then I see a list of all Invoice ids in the system
+# Each id links to the admin invoice show page
