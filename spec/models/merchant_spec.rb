24a7e2687657ae14045a23a3159c826a1766e5f1
Wil McCauley <wmccauley724@gmail.com>
2021-03-02 17:45:01 -0700

    Add feature and model tests for top sellign date mthod

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -132,4 +132,35 @@ RSpec.describe Merchant, type: :model do
       expect(Merchant.top_merchants).not_to include(@merchant6)
     end
   end
+
+  describe '#top_selling_date' do
+    before :each do
+      @merchant1 = create(:merchant)
+      @merchant2 = create(:merchant)
+
+      @item1 = create(:item, merchant_id: @merchant1.id)
+      @item2 = create(:item, merchant_id: @merchant1.id)
+      @item3 = create(:item, merchant_id: @merchant2.id)
+      @item4 = create(:item, merchant_id: @merchant2.id)
+
+      @invoice1 = create(:invoice, created_at: "2013-03-25 09:54:09 UTC")
+      @invoice2 = create(:invoice, created_at: "2012-03-17 09:54:09 UTC")
+      @invoice3 = create(:invoice, created_at: "2011-03-01 09:54:09 UTC")
+      @invoice4 = create(:invoice, created_at: "2020-03-25 09:54:09 UTC")
+
+      @invoice_item1 = create(:invoice_item, invoice_id: @invoice1.id, item_id: @item1.id, status: 1, quantity: 6, unit_price: 100)
+      @invoice_item2 = create(:invoice_item, invoice_id: @invoice2.id, item_id: @item2.id, status: 1, quantity: 2, unit_price: 100)
+      @invoice_item3 = create(:invoice_item, invoice_id: @invoice3.id, item_id: @item3.id, status: 0, quantity: 6, unit_price: 100)
+      @invoice_item4 = create(:invoice_item, invoice_id: @invoice4.id, item_id: @item4.id, status: 2, quantity: 2, unit_price: 100)
+
+      @transactions = create(:transaction, invoice_id: @invoice_item1.invoice.id, result: "success")
+      @transactions2 = create(:transaction, invoice_id: @invoice_item2.invoice.id, result: "success")
+      @transactions3 = create(:transaction, invoice_id: @invoice_item3.invoice.id, result: "success")
+      @transactions4 = create(:transaction, invoice_id: @invoice_item4.invoice.id, result: "success")
+    end
+    it 'returns a merchant top day by revenue' do
+      expect(@merchant1.top_selling_date).to eq("Monday, March 25, 2013")
+      expect(@merchant2.top_selling_date).to eq("Tuesday, March 01, 2011")
+    end
+  end
 end
