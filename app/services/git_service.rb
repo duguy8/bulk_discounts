23d090a9341d5fa7f791500a863dcc6357bc4e2f
Kris Litman <duguy8@gmail.com>
2021-03-04 21:03:53 -0500

    Added poro for holidays and updated gitservice to find upcoming 3 holidays in us

--- app/services/git_service.rb
+++ app/services/git_service.rb
@@ -1,4 +1,10 @@
 class GitService
+
+  def self.upcoming_holidays
+    input = get_data('https://date.nager.at/Api/v2/NextPublicHolidays/US')[0..2]
+    Holiday.upcoming(input)
+  end
+
   def self.repo_name
     get_data('https://api.github.com/repos/duguy8/little-esty-shop')[:name]
   end
