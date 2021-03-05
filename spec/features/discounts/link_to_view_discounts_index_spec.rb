2bb14d933301c4bbf59d9a795c7aa225d360a19d
Kris Litman <duguy8@gmail.com>
2021-03-04 20:01:49 -0500

    Added relationship to merchant model and built test for discounts index

---
+++ spec/features/discounts/link_to_view_discounts_index_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.describe "As a merchant on my dashboard" do
+  before :each do
+    @merchant = create(:merchant)
+
+    @discount1 = create(:discount, merchant_id: @merchant.id)
+    @discount2 = create(:discount, merchant_id: @merchant.id)
+    @discount3 = create(:discount, merchant_id: @merchant.id)
+  end
+
+  describe "I see a link to ~View All Discounts" do
+    it "Takes me to my discounts index page" do
+      visit merchants_dashboard_index_path(@merchant)
+
+      expect(page).to have_link("View All Discounts")
+
+      click_link "View All Discounts"
+
+      expect(current_path).to eq(merchant_discounts(@merchant))
+    end
+    it "I see all my discounts with their percentage & threshold"
+    it "Each discount includes a link to its show page"
+  end
+end
+
+
+
+
+# As a merchant
+# When I visit my merchant dashboard
+# Then I see a link to view all my discounts
+# When I click this link
+# Then I am taken to my bulk discounts index page
+# Where I see all of my bulk discounts including their
+# percentage discount and quantity thresholds
+# And each bulk discount listed includes a link to its show page
