088d7cb4bc03afe76bbf8077cdbba944c0b8a70b
Kris Litman <duguy8@gmail.com>
2021-03-06 14:02:51 -0500

    Adding in edge cases for discount revenue logic

--- spec/features/discounts/total_revenue_with_discounts_spec.rb
+++ spec/features/discounts/total_revenue_with_discounts_spec.rb
@@ -3,18 +3,21 @@ require "rails_helper"
 RSpec.describe "When I visit my merchant_invoice_show_page" do
   before :each do
     @merchant = create(:merchant)
+    @merchant2 = create(:merchant)
 
     @discount1 = create(:discount, merchant_id: @merchant.id, quantity_threshold: 10, percentage_discount: 20)
     @discount2 = create(:discount, merchant_id: @merchant.id, quantity_threshold: 12, percentage_discount: 10)
 
     @invoice = create(:invoice)
-
+    @invoice2 = create(:invoice)
 
     @item1 = create(:item, merchant_id: @merchant.id)
     @item2 = create(:item, merchant_id: @merchant.id)
 
     @invoice_item1 = create(:invoice_item, item_id: @item1.id, invoice_id: @invoice.id, quantity: 20, unit_price: 100)
     @invoice_item2 = create(:invoice_item, item_id: @item2.id, invoice_id: @invoice.id, quantity: 5, unit_price: 100)
+    @invoice_item3 = create(:invoice_item, item_id: @item1.id, invoice_id: @invoice2.id, quantity: 5, unit_price: 100)
+    @invoice_item4 = create(:invoice_item, item_id: @item2.id, invoice_id: @invoice2.id, quantity: 5, unit_price: 100)
   end
 
   describe "I see that the total revenue for my merchant" do
@@ -22,6 +25,12 @@ RSpec.describe "When I visit my merchant_invoice_show_page" do
       visit merchant_invoice_path(@merchant, @invoice)
 
       expect(page).to have_content("Total Revenue: $2100")
+    end
+
+    it "Should not apply discount if quantity threshold is not met" do
+      visit merchant_invoice_path(@merchant2, @invoice2)
+
+      expect(page).to have_content("Total Revenue: $1000")
     end
   end
 end
