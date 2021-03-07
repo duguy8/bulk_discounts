616da4f6bba96223b2a895fa57f0fbd49bf4c546
Kris Litman <duguy8@gmail.com>
2021-03-07 16:02:08 -0500

    Made model test more robust for invoice_item

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -20,26 +20,32 @@ RSpec.describe InvoiceItem, type: :model do
     before :each do
       @merchant = create(:merchant)
       @merchant2 = create(:merchant)
+      @merchant3 = create(:merchant)
 
       @discount1 = create(:discount, merchant_id: @merchant.id, quantity_threshold: 10, percentage_discount: 20)
       @discount2 = create(:discount, merchant_id: @merchant.id, quantity_threshold: 12, percentage_discount: 10)
       @discount3 = create(:discount, merchant_id: @merchant2.id, quantity_threshold: 10, percentage_discount: 20)
+      @discount4 = create(:discount, merchant_id: @merchant3.id, quantity_threshold: 10, percentage_discount: 10)
+      @discount5 = create(:discount, merchant_id: @merchant3.id, quantity_threshold: 10, percentage_discount: 20)
 
       @invoice = create(:invoice)
       @invoice2 = create(:invoice)
+      @invoice3 = create(:invoice)
 
       @item1 = create(:item, merchant_id: @merchant.id)
       @item2 = create(:item, merchant_id: @merchant.id)
-
       @item3 = create(:item, merchant_id: @merchant2.id)
+      @item4 = create(:item, merchant_id: @merchant3.id)
 
       @invoice_item1 = create(:invoice_item, item_id: @item1.id, invoice_id: @invoice.id, quantity: 20, unit_price: 100)
       @invoice_item2 = create(:invoice_item, item_id: @item2.id, invoice_id: @invoice.id, quantity: 5, unit_price: 100)
       @invoice_item3 = create(:invoice_item, item_id: @item3.id, invoice_id: @invoice2.id, quantity: 5, unit_price: 100)
+      @invoice_item4 = create(:invoice_item, item_id: @item4.id, invoice_id: @invoice3.id, quantity: 13, unit_price: 100)
     end
 
     it "#discount_applied" do
       expect(@invoice_item1.discount_applied).to eq(@discount1)
+      expect(@invoice_item4.discount_applied).to eq(@discount5)
       expect(@invoice_item2.discount_applied).to be nil
       expect(@invoice_item3.discount_applied).to be nil
     end
