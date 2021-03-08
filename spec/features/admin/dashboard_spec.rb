73888e6cf61ef0a81c0c925adb3d99afe2dddb8c
Kris Litman <duguy8@gmail.com>
2021-03-08 13:58:53 -0500

    finished styling

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe "When I visit '/admin' I see the admin dashboard" do
     VCR.use_cassette("github_information_five") do
     visit admin_index_path
 
-    expect(page).to have_content("This is the Admin Dashboard")
+    expect(page).to have_content("Admin Dashboard")
     expect(page).to have_link("Admin Merchants Index")
     expect(page).to have_link("Admin Invoices Index")
     end
