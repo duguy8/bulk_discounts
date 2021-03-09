255d1c72a22517d12a4c58b70eb6fde3e2f341a1
Kris Litman <duguy8@gmail.com>
2021-03-09 12:58:11 -0500

    Poro tests created and passing

--- app/controllers/customers/cart_controller.rb
+++ app/controllers/customers/cart_controller.rb
@@ -3,11 +3,16 @@ class Customers::CartController < ApplicationController
   before_action :set_item, only: [:update]
 
   def update
-    id = @item.id.to_s
-    session[:cart] ||= Hash.new(0)
-    session[:cart][id] ||= 0
-    session[:cart][id] = session[:cart][id] + 1
-    quantity = session[:cart][id]
+    # id = @item.id.to_s
+    # session[:cart] ||= Hash.new(0)
+    # session[:cart][id] ||= 0
+    # session[:cart][id] = session[:cart][id] + 1
+    # quantity = session[:cart][id]
+
+    @cart = Cart.new(session[:cart])
+    @cart.add_item(@item.id)
+    session[:cart] = @cart.contents
+    quantity = @cart.count_of(@item.id)
 
     flash[:notice] = "You now have #{pluralize(quantity, "#{@item.name}")} in your cart."
     redirect_to merchant_items_path(params[:merchant_id])
