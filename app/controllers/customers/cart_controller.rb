14d233681c432efdd9e0503c9f8cf7c6cd210a01
Kris Litman <duguy8@gmail.com>
2021-03-09 12:19:23 -0500

    Can have multiple items in cart, adding cart tracker

--- app/controllers/customers/cart_controller.rb
+++ app/controllers/customers/cart_controller.rb
@@ -1,8 +1,15 @@
 class Customers::CartController < ApplicationController
+  include ActionView::Helpers::TextHelper
   before_action :set_item, only: [:update]
 
   def update
-    flash[:notice] = "You now have 1 #{@item.name} in your cart."
+    id = @item.id.to_s
+    session[:cart] ||= Hash.new(0)
+    session[:cart][id] ||= 0
+    session[:cart][id] = session[:cart][id] + 1
+    quantity = session[:cart][id]
+
+    flash[:notice] = "You now have #{pluralize(quantity, "#{@item.name}")} in your cart."
     redirect_to merchant_items_path(params[:merchant_id])
   end
 
